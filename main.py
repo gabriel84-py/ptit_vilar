9dd07bfb1544efdc9029f7b6b280c64af44b3cd9
gabriel84-py <hexahack.g8430@gmail.com>
2025-10-20 19:50:26 +0200

    visitor ip héhé

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#main.py
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from routes import login, admin, index, articles_manage, articles, categories
--- models/visitors.py
+++ models/visitors.py
@@ -6,6 +6,7 @@ class Visitor(Base):
     __tablename__ = "visitors"
 
     id = Column(Integer, primary_key=True, index=True)
-    ip_address = Column(String(100), unique=True)  # l'adresse IP du visiteur
+    ip_address = Column(String(100), unique=True)  # adresse IP anonymisée
     user_agent = Column(String(255))               # info sur le navigateur
+    city = Column(String(100))                     # ville du visiteur
     first_visit = Column(DateTime, default=datetime.utcnow)
