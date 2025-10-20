422fe3b32f6798bf34ecc76e8267197ad5241679
gabriel84-py <hexahack.g8430@gmail.com>
2025-10-20 12:03:55 +0200

    commit du turfu

--- routes/index.py
+++ routes/index.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from services.article_admin import get_article, get_all_articles
 
 router = APIRouter(prefix="", tags=["racine"])
 
@@ -10,4 +11,5 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 def dashboard(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    articles = get_all_articles()
+    return templates.TemplateResponse("index.html", {"request": request, "article": articles})
